c944b7a6946eeade319bd5c186c78962b84671ca
elenarosed <rosedmersreg@gmail.com>
2022-08-08 11:05:46 +0300

    changes on resize

--- js/slider.js
+++ js/slider.js
@@ -4,8 +4,9 @@ let init = false;
 
 let brandsSection = document.querySelector('.brands-section');
 let brandsContainer = document.querySelector('.brands-container');
+let button = document.querySelector('.read-more-button');
 
-let swiperMode = function() {
+let layoutOnResize = function() {
     if(mobile.matches) {
         if (!init) {
             init = true;
@@ -26,18 +27,26 @@ let swiperMode = function() {
     else {
         swiper.destroy(true, true);
         init = false;
+
+        if (window.innerWidth >= 1120 && button.classList.contains('read-more-button_opened')) {
+            brandsSection.style.height = '412px';
+            brandsContainer.style.height = '288px';
+        }
+        else if (window.innerWidth >= 768 && button.classList.contains('read-more-button_opened')) {
+            brandsSection.style.height = '500px';
+            brandsContainer.style.height = '376px';
+        }
     }
 }
-window.addEventListener("load", swiperMode);
-window.addEventListener("resize", swiperMode);
+window.addEventListener("load", layoutOnResize);
+window.addEventListener("resize", layoutOnResize);
 
 let showMore = function(){
-    let button = document.querySelector('.read-more-button');
     let buttonStyles = document.querySelector('.brands-section__button');
     let showMore = button.classList.contains('read-more-button_closed');
 
     if(showMore){
-        if (window.innerWidth > 1120) {
+        if (window.innerWidth >= 1120) {
             brandsSection.style.height = '412px';
             brandsContainer.style.height = '288px';
         }
